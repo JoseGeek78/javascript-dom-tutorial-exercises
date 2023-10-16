b2818d38384d402b5d982e98024647892418598f
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-16 19:20:53 +0000

    Ejercicios

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,16 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	 // Crear un nuevo elemento div
+	 
+     let newDiv = document.createElement('div');
+	 // Establecer el fondo amarillo
+	 
+     newDiv.style.backgroundColor = 'yellow';
+	 // Establecer el texto interno (innerHTML)
 	
+     newDiv.innerHTML = 'Hello World';
+	 // Agregar el nuevo div al cuerpo (body) del sitio web
+	 
+	 document.body.appendChild(newDiv);
 });
--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,14 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+ // Crea un nuevo elemento li
+ let newLi = document.createElement('li');
+ // Agrega contenido al nuevo elemento li (puedes cambiar esto según tu necesidad)
+ newLi.textContent = 'Fourth element';
+ // Obtén la lista #myList
 
+ let mylist = document.querySelector('#myList');
+ // Añade el nuevo elemento li a la lista como hijo
+
+ myList.appendChild(newLi);
 });
