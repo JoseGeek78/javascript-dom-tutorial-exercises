7ddd815bf8fc6fa5595ebfcb377be39eb3d7f685
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-17 04:27:36 +0000

    Ejercicios

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,21 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
-// your code here
+
+// Obtén la referencia al elemento select con el id 'mySelect'
+const selectElement = document.getElementById('mySelect');
+
+// Itera a través del arreglo y agrega cada país como una opción al select
+countries.forEach(country => {
+  const option = document.createElement('option');
+  option.value = country;
+  option.text = country;
+  selectElement.appendChild(option);
+});
+
+// Agrega un oyente de eventos para el evento 'change'
+selectElement.addEventListener('change', function() {
+  // Obtiene el país seleccionado
+  const selectedCountry = selectElement.value;
+  
+  // Muestra una alerta con el país seleccionado
+  alert(`Has seleccionado: ${selectedCountry}`);
+});
--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,35 @@
 // Your code here
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtén elementos del DOM
+    const todoList = document.querySelector('ul');
+    const input = document.getElementById('addToDo');
+  
+    // Agrega tarea
+    input.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter' && input.value.trim() !== '') {
+        const taskText = input.value;
+        input.value = '';
+        const listItem = document.createElement('li');
+        const deleteButton = document.createElement('span');
+  
+        listItem.innerHTML = `<span><i class="fa fa-trash"></i></span> ${taskText}`;
+        deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
+        listItem.appendChild(deleteButton);
+        todoList.appendChild(listItem);
+  
+        // Agrega la funcionalidad de eliminar la tarea
+        deleteButton.addEventListener('click', function () {
+          todoList.removeChild(listItem);
+        });
+      }
+    });
+  
+    // Elimina tarea
+    todoList.addEventListener('click', function (e) {
+      if (e.target && e.target.tagName === 'I' && e.target.className === 'fa fa-trash') {
+        const listItem = e.target.parentElement.parentElement;
+        todoList.removeChild(listItem);
+      }
+    });
+  });
+  
